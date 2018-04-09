5f16735fa68f37c9bd71a1637b3a361b7d7def41
nmlgc <nmlgc@nmlgc.net>
2018-04-09 17:30:48 +0200

    [Reduction] Identify and remove the C runtime
    It's amazing how almost impossible it was to find *any* version of
    MAKER.EXE whose libc exactly matches *any* version of a Borland
    compiler. So, Borland C++ 5.01 it is, exactly. 4.52 is too old, and 5.02
    is too new.
    
    And yay! We can indeed debug MAKER40D.EXE in Turbo Debugger now.

--- build16b.bat
+++ build16b.bat
@@ -6,17 +6,22 @@ tasmx 1>NUL 2>NUL
 if errorlevel 9009 goto no_tasmx
 if errorlevel 216 goto 64_bit
 
-tlink >NUL
-if errorlevel 9009 goto no_tlink
+bcc >NUL
+if errorlevel 9009 goto no_bcc
 if errorlevel 216 goto 64_bit
 
 REM Build
 REM -----
 
-tasmx /m /kh32768 /t MAKER40.ASM, MAKER40.OBJ
-tasmx /m /kh32768 /t /zi MAKER40.ASM, MAKER40D.OBJ
-tlink MAKER40.OBJ
-tlink /v MAKER40D.OBJ
+REM TASM under DOSBox seems to need that, because for some reason it can't
+REM write to those once they exist -.-
+del MAKER40.OBJ
+del MAKER40D.OBJ
+
+tasmx /m /mx /kh32768 /t MAKER40.ASM, MAKER40.OBJ
+tasmx /m /mx /kh32768 /t /zi MAKER40.ASM, MAKER40D.OBJ
+bcc -mc -ls MAKER40.OBJ
+bcc -mc -ls -v MAKER40D.OBJ
 goto eof
 
 REM Errors
@@ -27,12 +32,11 @@ goto eof
 
 :no_tasmx
 echo Could not find TASMX.
-goto tc5_bin
-:no_tlink
-echo Could not find TLINK.
-goto tc5_bin
-:tc5_bin
 echo Please make sure that the BIN directory of Turbo Assembler 5.0 is in your PATH.
+goto eof
+:no_bcc
+echo Could not find BCC.
+echo Please make sure that the BIN directory of Borland C++ 5.01 is in your PATH.
 goto eof
 
 :eof
